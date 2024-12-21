c3bd679ee1e106f82c07dff86467ffec6028765a
tmx315 <tmx12531574121@qq.com>
2024-12-21 17:50:05 +0800

    Delete 014 directory

--- 014/src/config/wallpaper.js
+++
@@ -1,9 +0,0 @@
-let wallpaper = [
-  'bg_default.jpg'
-]
-
-wallpaper = wallpaper.map(item => {
-  return 'https://cdn.jsdelivr.net/gh/warpprism/cdn@latest/autopiano/static/images/' + item
-})
-
-export default wallpaper
